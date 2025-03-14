40a0f828da2966c20296b1bd22ed85f54a96e14b
Sanjidul <Sanjidul>
2025-03-14 19:14:14 +0600

    Add ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,36 @@ import { AppComponent } from './app.component';
 import { StudiosComponent } from './studios/studios.component';
 import { provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BookingDialogComponent } from './studios/booking-dialog/booking-dialog.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTimepickerModule} from '@angular/material/timepicker';
+
+import {provideNativeDateAdapter} from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudiosComponent
+    StudiosComponent,
+    BookingDialogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatTimepickerModule
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [provideHttpClient(withInterceptorsFromDi()), provideNativeDateAdapter()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
