756d9ecc97a2044d8208d113eb5ee59e114a6de6
Sanjidul <Sanjidul>
2025-03-14 23:40:18 +0600

    Add name, email and date validation

--- src/app/studios/booking-dialog/booking-dialog.component.ts
+++ src/app/studios/booking-dialog/booking-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-booking-dialog',
@@ -12,19 +12,19 @@ export class BookingDialogComponent {
    dialogRef = inject(MatDialogRef<BookingDialogComponent>);
    data = inject(MAT_DIALOG_DATA);
    bookingForm = null;
+   today= new Date();
    openingHour = '';
    closingHour = '';
 
    constructor() {
-     console.log(this.data);
      this.openingHour = this.data?.Availability?.Open;
      this.closingHour = this.data?.Availability?.Close;
      this.bookingForm = new FormGroup({
-       name: new FormControl(null),
-       email: new FormControl(null),
-       date: new FormControl(null),
-       startTime: new FormControl(null),
-       endTime: new FormControl(null)
+       name: new FormControl(null, Validators.required),
+       email: new FormControl(null, Validators.required),
+       date: new FormControl(null, Validators.required),
+       startTime: new FormControl(null, Validators.required),
+       endTime: new FormControl(null, Validators.required)
      });
    }
 
